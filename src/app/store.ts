8bb84f8ece2906027d75795eb3eeb8181f5134b5
Anartz Mugika <mugan86@gmail.com>
2017-03-27 15:44:42 +0200

    Add increment constant

--- src/app/store.ts
+++ src/app/store.ts
@@ -9,7 +9,7 @@ export interface IAppState
 export function rootReducer(state:IAppState, action):IAppState
 {
     switch(action.type) {
-        case 'INCREMENT': return { counter: state.counter + 1}
+        case INCREMENT: return { counter: state.counter + 1}
     }
     return state;
 }
