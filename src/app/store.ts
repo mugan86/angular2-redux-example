49ecd346630c15015364f11855227fbbc347cb60
Anartz Mugika <mugan86@gmail.com>
2017-03-28 08:36:44 +0200

    Replace basic return with Object.assign...

--- src/app/store.ts
+++ src/app/store.ts
@@ -19,7 +19,9 @@ export const INITIAL_STATE : IAppState = {
 export function rootReducer(state:IAppState, action):IAppState
 {
     switch(action.type) {
-        case INCREMENT: return { counter: state.counter + 1}
+        case INCREMENT: 
+        //return { counter: state.counter + 1}
+        return Object.assign({}, state, {counter: state.counter + 1});
     }
     return state;
 }
