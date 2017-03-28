5d79688626f6a64a1ed59d659b1cdabd23f4f7f4
Anartz Mugika <mugan86@gmail.com>
2017-03-28 08:25:46 +0200

    Add new value in IAppState interface and initialize value inside INITIAL_STATE constant

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,17 +3,23 @@ export interface IAppState
 {
     //To use in counter
     counter: number;
+    messaging: {
+        newMessages: number;
+    }
 }
 
 export const INITIAL_STATE : IAppState = {
-    counter : 0
+    counter : 0,
+    messaging: {
+        newMessages: 5
+    }
 }
 
 //Different states to make actions
 export function rootReducer(state:IAppState, action):IAppState
 {
     switch(action.type) {
-        case INCREMENT: return { counter: state.counter + 1}
+        case INCREMENT: return { counter: state.counter + 1, messaging}
     }
     return state;
 }
