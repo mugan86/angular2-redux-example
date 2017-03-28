42b67ab1919f1aec5c3f025ff7c3b7387eca34bb
Anartz Mugika <mugan86@gmail.com>
2017-03-28 09:01:42 +0200

    Import Map and fromJS (immutable) and change contructor param and inside function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ import { IAppState, rootReducer, INITIAL_STATE} from './store';
   bootstrap: [AppComponent]
 })
 export class AppModule { 
-  constructor(ngRedux: NgRedux<IAppState>)
+  constructor(ngRedux: NgRedux<Map<string, any>>)
   {
     //Pass counter = 0 in state to change when change state in rootReducer of store.ts
     ngRedux.configureStore(rootReducer, fromJS(INITIAL_STATE));
