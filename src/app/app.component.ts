72ea6b469b0805abec4640430700c962c3e3f332
Anartz Mugika <mugan86@gmail.com>
2017-03-28 13:52:21 +0200

    Define decrement function to dispatch DECREMENT type to -1 operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NgRedux, select/*Add select decorator*/} from 'ng2-redux';
 import { IAppState } from './store';
-import { INCREMENT } from './actions';
+import { INCREMENT, DECREMENT } from './actions';
 import { Map } from 'immutable';
 
 @Component({
@@ -18,5 +18,9 @@ export class AppComponent {
   increment() {
     //this.counter++; //Argument mutating
     this.ngRedux.dispatch({type: INCREMENT});
+  }
+
+  decrement() {
+    this.ngRedux.dispatch({type: DECREMENT})
   }
 }
