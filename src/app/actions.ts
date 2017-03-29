1398e8bd4385dbaf5ab37c2f30aecbd829ce9c8c
Anartz Mugika <mugan86@gmail.com>
2017-03-29 10:18:39 +0200

    Add RESET action

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,6 +1,4 @@
 //NOt add line
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
-
-export const ADD_ITEM = 'ADD_ITEM';
-export const REMOVE_ITEM = 'REMOVE_ITEM';
+export const RESET = 'RESET';
