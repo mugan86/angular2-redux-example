a636f724dd9ddb5cf37652d9f80ccdc73b299a56
Anartz Mugika <mugan86@gmail.com>
2017-03-29 10:19:53 +0200

    Add function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NgRedux, select/*Add select decorator*/} from 'ng2-redux';
 import { IAppState } from './store';
-import { INCREMENT, DECREMENT } from './actions';
+import { INCREMENT, DECREMENT, RESET } from './actions';
 import { Map } from 'immutable';
 
 @Component({
@@ -22,5 +22,10 @@ export class AppComponent {
 
   decrement() {
     this.ngRedux.dispatch({type: DECREMENT})
+  }
+
+  reset() {
+    //this.counter++; //Argument mutating
+    this.ngRedux.dispatch({type: RESET});
   }
 }
