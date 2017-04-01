056cedb4ce473497091dbc3872c3bbcf0a29a18c
Anartz Mugika <mugan86@gmail.com>
2017-04-01 12:58:54 +0200

    Add KeyEvents component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,10 +13,12 @@ import { fromJS, Map } from 'immutable';
 import { IAppState, rootReducer, INITIAL_STATE} from './store';
 
 import { AppComponent } from './app.component';
+import { KeyEvents } from './components/key-events/key-events';
 
 @NgModule({
   declarations: [
     AppComponent,
+    KeyEvents
   ],
   imports: [
     BrowserModule,
@@ -27,7 +29,7 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { 
+export class AppModule {
   constructor(ngRedux: NgRedux<Map<string, any>>)
   {
     //Pass counter = 0 in state to change when change state in rootReducer of store.ts
