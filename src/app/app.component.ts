e69ab79458d82c55f91a765e88da9d3bcfb92596
Anartz Mugika <mugan86@gmail.com>
2017-04-03 08:46:03 +0200

    Remove keyevents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgRedux, select/*Add select decorator*/} from 'ng2-redux';
 import { IAppState } from './store';
 import { INCREMENT, DECREMENT, RESET, HALF, DUPLICATE } from './actions';
@@ -48,57 +48,5 @@ export class AppComponent {
     else this.valueZero = true;
   }
 
-  /* a few examples */
-  keyboardEvent: any;
-  altKey: boolean;
-  charCode: number;
-  code: string;
-  ctrlKey: boolean;
-  keyCode: number;
-  keyIdentifier: string;
-  metaKey: boolean;
-  shiftKey: boolean;
-  timeStamp: number;
-  type: string;
-  which: number;
-
-  @HostListener('window:keydown', ['$event'])
-  keyboardInput(event: any) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    this.keyboardEvent = event;
-    this.altKey = event.altKey;
-    this.charCode = event.charCode;
-    this.code = event.code;
-    this.ctrlKey = event.ctrlKey;
-    this.keyCode = event.keyCode;
-    this.keyIdentifier = event.keyIdentifier;
-    this.metaKey = event.metaKey;
-    this.shiftKey = event.shiftKey;
-    this.timeStamp = event.timeStamp;
-    this.type = event.type;
-    this.which = event.which;
-
-    if (this.code == 'ArrowUp' || this.keyCode == 38)
-    {
-      this.increment();
-    }
-    else if (this.code == 'ArrowRight' || this.keyCode == 39)
-    {
-      this.duplicate();
-    }
-    else if (this.code == 'ArrowLeft' || this.keyCode == 37)
-    {
-      this.half();
-    }
-    else if (this.code == 'ArrowDown' || this.keyCode == 40)
-    {
-      this.decrement();
-    }
-    else if (this.code == 'KeyC' || this.keyCode == 67)
-    {
-      this.reset();
-    }
-  }
+  
 }
